41b911b6c486452d1b04d0c3f39b81f5ea6151c7
Feliss-dev <thanhlta1234@gmail.com>
2024-11-24 16:35:15 +0700

    create channel functionality

--- src/components/modals.tsx
+++ src/components/modals.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { CreateChannelModal } from "@/features/channels/components/create-channel-modal";
 import { CreateWorkspaceModal } from "@/features/workspaces/components/create-workspace-modal";
+
 import { useEffect, useState } from "react";
 
 export const Modals = () => {
@@ -13,6 +15,7 @@ export const Modals = () => {
   if (!mounted) return null;
   return (
     <>
+      <CreateChannelModal/>
       <CreateWorkspaceModal />
     </>
   );
