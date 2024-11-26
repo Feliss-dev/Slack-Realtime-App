89a7878a291ca605e10cb1b2450c9f7e32c77d55
Feliss-dev <thanhlta1234@gmail.com>
2024-11-26 09:53:15 +0700

    custom layout

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,10 +1,27 @@
-import Editor from '@/components/ui/editor'
-import React from 'react'
+import dynamic from 'next/dynamic'
+import { platform } from 'os';
+import Quill from 'quill';
 
-export const ChatInput = () => {
+import React, { useRef } from 'react'
+
+const Editor = dynamic(() => import("@/components/ui/editor"), { ssr: false });
+
+interface ChatInputProps {
+  placeholder?: string;
+}
+
+export const ChatInput = ({placeholder}: ChatInputProps) => {
+  const editorRef = useRef<Quill | null>(null);
+
+  
   return (
     <div className='px-5 w-full'>
-        <Editor/>
+        <Editor 
+        placeholder={placeholder}
+        onSubmit={()=> {}}
+        disabled={false}
+        innerRef={editorRef}
+        />
         </div>
   )
 }
--- src/components/ui/editor.tsx
+++ src/components/ui/editor.tsx
@@ -1,9 +1,57 @@
-import React, { useEffect, useRef } from 'react'
+import React, { MutableRefObject, useEffect, useLayoutEffect, useRef, useState } from 'react'
 import Quill, { QuillOptions } from "quill";
 import "quill/dist/quill.snow.css";
+import { Button } from './button';
+import {PiTextAa} from "react-icons/pi"
+import { ImageIcon, Smile } from 'lucide-react';
+import {MdSend} from "react-icons/md"
+import { Hint } from '../hint';
+import { Delta, Op } from 'quill/core';
 
-const Editor = () => {
+type EditorValue = {
+    image: File | null;
+    body: string;
+}
+
+interface EditorProps {
+
+    onSubmit: ({image, body}: EditorValue) => void;
+    onCancel?: () => void;
+    placeholder?: string;
+    defaultValue?: Delta | Op[];
+    disabled?: boolean;
+    innerRef?: MutableRefObject<Quill | null>;
+    variant?: "create" | "update";
+};    
+
+
+const Editor = ({
+    onCancel,
+    onSubmit,
+    placeholder = "Write something...",
+    defaultValue = [],
+    innerRef,
+    variant = "create"}: EditorProps) => {
+
+    const [text, setText] = useState("");
+
     const containerRef = useRef<HTMLDivElement>(null);
+    
+    const submitRef = useRef(onSubmit);
+    
+    const placeholderRef = useRef(placeholder);
+
+    const quillRef = useRef<Quill | null>(null);
+    const disabledRef = useRef(false);
+    const defaultValueRef = useRef(defaultValue);
+
+   useLayoutEffect(() => {
+    submitRef.current = onSubmit;
+    placeholderRef.current = placeholder;
+    disabledRef.current = false;
+    defaultValueRef.current = defaultValue;
+   })
+
 
     useEffect(() => {
         if(!containerRef.current) return;
@@ -15,12 +63,101 @@ const Editor = () => {
 
         const options: QuillOptions = {
             theme: "snow",
+            placeholder: placeholderRef.current,
+
+        };
+
+        const quill = new Quill(editorContainer, options);
+        quillRef.current = quill;
+        quillRef.current.focus();
+
+        if(innerRef){
+            innerRef.current = quill;
         }
-    }, []);
+
+        quill.setContents(defaultValueRef.current);
+        return () => {
+            if(container){
+                container.innerHTML = "";
+            }
+        };
+    }, [innerRef]);
   return (
     <div className="flex flex-col">
         <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm bg-white">
-            <div>Editor</div>
+            <div ref={containerRef}
+                className='h-full ql-custom'
+            />
+            <div className="flex px-2 pb-2 z-[5]">
+                <Hint label="Hide formating" >
+                <Button
+                disabled={false}
+                size="iconSm"
+                variant="ghost"
+                onClick={() => {}}
+                >
+                    <PiTextAa className='size-4'/>
+                </Button>
+
+                </Hint>
+               
+               <Hint label="Emoji"> 
+                <Button
+                disabled={false}
+                size="iconSm"
+                variant="ghost"
+                onClick={() => {}}
+                >
+                    <Smile className='size-4'/>
+                </Button>
+                </Hint>
+               
+               {variant === "create" &&(
+                 <Hint label="Image">
+                <Button
+                disabled={false}
+                size="iconSm"
+                variant="ghost"
+                onClick={() => {}}
+                >
+                    <ImageIcon className='size-4'/>
+                </Button>
+                </Hint> )}
+                
+                {variant === "update" &&(
+                    <div className="ml-auto flex items-center gap-x-2 ">
+                        <Button variant="outline" size="sm" onClick={() => {}} disabled={false}>
+                            Cancel
+                        </Button>
+
+                        <Button
+                        disabled={false}
+                        onClick={() => {}}
+                        size="sm"
+                        className=" bg-[#007a5a] hover:bg-[#007a5a]/80 text-white"
+                        >
+                            Save
+                        </Button>
+                    </div>
+                )}
+
+                {variant === "create" &&(
+                     <Button
+                     disabled={false}
+                     onClick={() => {}}
+                     size="iconSm"
+                     className="ml-auto bg-[#007a5a] hover:bg-[#007a5a]/80 text-white">
+                         <MdSend className="size-4"/>
+                     </Button>
+                )}
+               
+                
+            </div>
+        </div>
+        <div className="p-2 text-[10px] text-muted-foreground flex justify-end">
+            <p>
+                <strong>Shift + Return</strong> to add a new line
+            </p>
         </div>
     </div>
   )
